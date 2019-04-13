eb5603cf9a496de589b99a7c7794891670e2fc7f
infinitypaul <infinitypaul@live.com>
2019-04-13 18:42:07 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Modal from '../src/UI/Modal/Modal'
 
 class App extends Component {
+  state={
+    modalToggle: false
+  }
+
+  modalHandler = (e) => {
+    e.preventDefault();
+    this.setState({
+      modalToggle: !this.state.modalToggle
+    })
+  }
   render() {
     return (
       <div className="App">
@@ -15,10 +26,18 @@ class App extends Component {
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
+            onClick={this.modalHandler}
             rel="noopener noreferrer"
           >
             Learn React
           </a>
+          <Modal show={this.state.modalToggle} modalClosed={this.modalHandler}>
+            <div
+                style={{color:'black'}}
+            >
+              The Best Has Happened To ME
+            </div>
+          </Modal>
         </header>
       </div>
     );
